8c36f90635c588257700ed6d96be273b67500c0e
D.hacThe <hienthe473@gmail.com>
2022-05-23 11:48:16 +0700

    merge thanh

--- src/views/pages/auth/sign-up/SignUp.js
+++ src/views/pages/auth/sign-up/SignUp.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 
 function SignUp(props) {
+
+  const foodlist = useSelector(state=>state.food.foods)
+  const id = foodlist.indexOf("Bánh ngọt") + 1
   return (
     <div>
       
